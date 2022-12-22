bcc16ea19799b2da6bdee1100fc9a38aee012c1f
Paulin J <paulin.juhen@outlook.com>
2022-12-22 19:16:27 +0100

    Ajout d'un modal(bootstrap) pour le formulaire de connexion

--- site-reseau/src/app/menu-nav/menu-nav.component.ts
+++ site-reseau/src/app/menu-nav/menu-nav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-menu-nav',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu-nav.component.css']
 })
 export class MenuNavComponent {
+  // ngOnInit() {
+  //   console.log("ngOnInit()");
+    
+  //   const forms: NodeListOf<HTMLFormElement> = document.querySelectorAll('.needs-validation')
 
+  //   Array.from(forms).forEach(form => {
+  //     form.addEventListener('submit', event => {
+  //       if (!form.checkValidity()) {
+  //         event.preventDefault()
+  //         event.stopPropagation()
+  //       }
+
+  //       form.classList.add('was-validated')
+  //     }, false)
+  //   })
+  // }
+
+  loginForm = new FormGroup({
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
+
+  get username() { return this.loginForm.get('username'); }
+  get password() { return this.loginForm.get('password'); }
+
+  onSubmit() {
+    console.warn(this.loginForm.status);    
+  }
 }
